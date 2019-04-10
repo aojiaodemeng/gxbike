c2bfff7d3aa0e1b91cec881fd1d01d3b46b810cd
chenxinyi <chenxinyi@chaintor.com>
2019-04-10 17:29:25 +0800

    底部组件开发完成

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import './index.less'
 export default class Footer extends React.Component{
     render(){
         return (
-            <div>
-                Footer
+            <div className="footer">
+                版权所有：傲娇的萌
             </div>
         )
     }
